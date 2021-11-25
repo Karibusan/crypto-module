be4718a0a513b81500def5f5c460a5cf248d77e3
Yan Urquiza <yan.urquiza@gmail.com>
2021-11-25 15:15:27 +0000

    symfony 5 :)

---
+++ application/src/Controller/CiphersController.php
@@ -0,0 +1,21 @@
+<?php
+	namespace App\Controller;
+	
+	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\Routing\Annotation\Route;
+	
+	class CiphersController extends AbstractController
+	{
+		/**
+		 * @Route("/ciphers/substitution")
+		 */
+		public function number(): Response
+		{
+			$number = random_int(0, 100);
+			
+			return $this->render('ciphers/substitution.html.twig', [
+				'number' => $number,
+			]);
+		}
+	}
