73f8b86b6e7ec1aed4f1506c364fabc7c950efab
Yan Urquiza <yan.urquiza@gmail.com>
2021-11-25 19:38:50 +0000

    LETTERS FREQUENCY

--- application/public/javascript/ciphers/substitution/substitution.js
+++ application/public/javascript/ciphers/substitution/substitution.js
@@ -2,135 +2,207 @@ let longestWordText = null;
 let pattern = '';
 let words = [];
 let matches = [];
+let alphabet = [
+	'A', 'B', 'C', 'D', 'E', 'F',
+	'G', 'H', 'I', 'J', 'K', 'L',
+	'M', 'N', 'O', 'P', 'Q', 'R',
+	'S', 'T', 'U', 'V', 'W', 'X',
+	'Y', 'Z'];
 
-$(function () {
-    $('#longest-word-ta-btn').prop('disabled', true);
+$ (function () {
+	$ ('#longest-word-ta-btn').prop ('disabled', true);
 });
 
-$('#longest-word-ta-btn').on('click', function () {
-    longestWordText = $('#longest-word-ta').val();
-    removePunctuation();
-    longestWord();
+$ ('#longest-word-ta-btn').on ('click', function () {
+	longestWordText = $ ('#longest-word-ta').val ();
+	removePunctuation ();
+	longestWord ();
 });
 
-$('#longest-word-ta').bind('paste keydown', function (e) {
-    reset();
-    let eventType = e.type;
-    if (eventType === 'paste') {
-        longestWordText = e.originalEvent.clipboardData.getData('text');
-    } else {
-        longestWordText = $('#longest-word-ta').val();
-    }
-    if (longestWordText.length > 1) {
-        $('#longest-word-ta-btn').prop('disabled', false);
-
-    } else {
-        $('#longest-word-ta-btn').prop('disabled', true);
-    }
+$ ('#longest-word-ta').bind ('paste keydown', function (e) {
+	reset ();
+	let eventType = e.type;
+	if (eventType === 'paste') {
+		longestWordText = e.originalEvent.clipboardData.getData ('text');
+	} else {
+		longestWordText = $ ('#longest-word-ta').val ();
+	}
+	if (longestWordText.length > 1) {
+		$ ('#longest-word-ta-btn').prop ('disabled', false);
+		
+	} else {
+		$ ('#longest-word-ta-btn').prop ('disabled', true);
+	}
 });
 
-function removePunctuation() {
-    let punctuationless = longestWordText.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
-    longestWordText = punctuationless.replace(/\s{2,}/g, " ");
+function removePunctuation () {
+	let punctuationless = longestWordText.replace (/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+	longestWordText = punctuationless.replace (/\s{2,}/g, " ");
 }
 
-function longestWord() {
-    let str = longestWordText.split(" ");
-    let longest = 0;
-    let maxLength = 0;
-    str.forEach(function (str) {
-        if (longest < str.length) {
-            longest = str.length;
-            maxLength = longest;
+function longestWord () {
+	let str = longestWordText.split (" ");
+	let longest = 0;
+	let maxLength = 0;
+	str.forEach (function (str) {
+		if (longest < str.length) {
+			longest = str.length;
+			maxLength = longest;
+		}
+	});
+	str.forEach (function (str) {
+		if (maxLength === str.length) {
+			words.push (str);
+		}
+	});
+	//console.log(words);
+	getPattern ();
+	searchObj ();
+	populateResults ();
+	populateMatches ();
+    frequencyAnalysis();
+	reset ();
+	//  console.log('MATCHES' , matches);
+}
+
+function reset () {
+	longestWordText = null;
+	pattern = '';
+	words = [];
+	matches = [];
+}
+
+function getPattern () {
+	for (let key in words) {
+		//  let pattern = [];
+		let lastCharacterUsedIndex = 0;
+		let word = words[key];
+		let reconstruct = '';
+		//  console.log('WORD' , word);
+		
+		for (let i = 0; i < word.length; i++) {
+			if (reconstruct.indexOf (word.charAt (i)) > -1) {
+				var characterPosition = reconstruct.indexOf (word.charAt (i));
+				//  console.log('characterPosition' , characterPosition);
+				var character = pattern.charAt (characterPosition);
+				//  console.log('character' , character);
+				pattern += character;
+			} else {
+				pattern += String.fromCharCode (lastCharacterUsedIndex + 65);
+				lastCharacterUsedIndex += 1;
+			}
+			reconstruct += word.charAt (i);
+			// console.log('patternString' , pattern);
+			//  console.log('RECONSTRUCT' , reconstruct);
+		}
+	}
+	
+}
+
+function searchObj () {
+	for (var key in dictionary) {
+		var value = dictionary[key];
+		if (typeof value === 'object') {
+			searchObj (value, pattern);
+		}
+		if (value === pattern) {
+			matches[key] = value;
+		}
+	}
+}
+
+
+function populateResults () {
+	$ ("#words-table-content").empty ();
+	// console.log('WORDS' , words);
+	for (var key in words) {
+		var word = words[key];
+		// console.log(word);
+		let tr;
+		tr = $ ('<tr/>');
+		tr.append ("<td>" + word + "</td>");
+		tr.append ("<td>" + word.length + "</td>");
+		tr.append ("<td>" + pattern + "</td>");
+		$ ('#words-table').append (tr);
+	}
+}
+
+function populateMatches () {
+	$ ("#matches-table-content").empty ();
+	// console.log('MATCHES' , matches);
+	for (var key in matches) {
+		var pattern = matches[key];
+		var wordMatched = key;
+		// console.log(wordMatched);
+		let tr;
+		tr = $ ('<tr/>');
+		tr.append ("<td>" + pattern + "</td>");
+		tr.append ("<td>" + wordMatched + "</td>");
+		$ ('#matches-table').append (tr);
+	}
+}
+
+function frequencyAnalysis () {
+    let spaceOutString = longestWordText.replace(/\s/g, '');
+    spaceOutString = spaceOutString.toUpperCase();
+	let textlength = spaceOutString.length;
+	let alphabetLength = alphabet.length;
+    let countCharacters = {};
+    console.log('textlength' , textlength);
+    console.log('alphabetLength' , alphabetLength);
+    for (let i = 0; i < textlength; i++) {
+        let char = spaceOutString.charAt (i);
+        if( char in countCharacters) {
+            countCharacters[char] += 1;
         }
-    });
-    str.forEach(function (str) {
-        if (maxLength === str.length) {
-            words.push(str);
-        }
-    });
-    //console.log(words);
-    getPattern();
-    searchObj();
-    populateResults();
-    populateMatches();
-    reset();
-    //  console.log('MATCHES' , matches);
-}
-
-function reset() {
-    longestWordText = null;
-    pattern = '';
-    words = [];
-    matches = [];
-}
-
-function getPattern() {
-    for (let key in words) {
-        //  let pattern = [];
-        let lastCharacterUsedIndex = 0;
-        let word = words[key];
-        let reconstruct = '';
-        //  console.log('WORD' , word);
-
-        for (let i = 0; i < word.length; i++) {
-            if (reconstruct.indexOf(word.charAt(i)) > -1) {
-                var characterPosition = reconstruct.indexOf(word.charAt(i));
-                //  console.log('characterPosition' , characterPosition);
-                var character = pattern.charAt(characterPosition);
-                //  console.log('character' , character);
-                pattern += character;
-            } else {
-                pattern += String.fromCharCode(lastCharacterUsedIndex + 65);
-                lastCharacterUsedIndex += 1;
-            }
-            reconstruct += word.charAt(i);
-            // console.log('patternString' , pattern);
-            //  console.log('RECONSTRUCT' , reconstruct);
+        else
+        {
+            countCharacters[char] = 1;
         }
     }
-
-}
-
-function searchObj() {
-    for (var key in dictionary) {
-        var value = dictionary[key];
-        if (typeof value === 'object') {
-            searchObj(value, pattern);
-        }
-        if (value === pattern) {
-            matches[key] = value;
-        }
+    
+    for (var key in countCharacters) {
+        var character = key;
+        var count = countCharacters[key];
+        var frequency = ( count / textlength)*100;
+	    countCharacters[key] = frequency;
     }
-}
-
-
-function populateResults() {
-    $("#words-table-content").empty();
-    // console.log('WORDS' , words);
-    for (var key in words) {
-        var word = words[key];
-        // console.log(word);
-        let tr;
-        tr = $('<tr/>');
-        tr.append("<td>" + word + "</td>");
-        tr.append("<td>" + word.length + "</td>");
-        tr.append("<td>" + pattern + "</td>");
-        $('#words-table').append(tr);
-    }
-}
-
-function populateMatches() {
-    $("#matches-table-content").empty();
-    // console.log('MATCHES' , matches);
-    for (var key in matches) {
-        var pattern = matches[key];
-        var wordMatched = key;
-        // console.log(wordMatched);
-        let tr;
-        tr = $('<tr/>');
-        tr.append("<td>" + pattern + "</td>");
-        tr.append("<td>" + wordMatched + "</td>");
-        $('#matches-table').append(tr);
-    }
+	
+	google.charts.load('current', {packages: ['corechart', 'bar']});
+	google.charts.setOnLoadCallback(drawBasic);
+	
+	function drawBasic() {
+		
+		var data = new google.visualization.DataTable();
+		data.addColumn('string', 'Letter');
+		data.addColumn('number', 'Frequency');
+		
+		data.addRows([
+			['A', countCharacters['A']], ['B', countCharacters['B']], ['C', countCharacters['C']],
+			['D', countCharacters['D']], ['E', countCharacters['E']], ['F', countCharacters['F']],
+			['G', countCharacters['G']], ['H', countCharacters['H']], ['I', countCharacters['I']],
+			['J', countCharacters['J']], ['K', countCharacters['K']], ['L', countCharacters['L']],
+			['M', countCharacters['M']], ['N', countCharacters['N']], ['O', countCharacters['O']],
+			['P', countCharacters['P']], ['Q', countCharacters['Q']], ['R', countCharacters['R']],
+			['S', countCharacters['S']], ['T', countCharacters['T']], ['U', countCharacters['U']],
+			['V', countCharacters['V']], ['W', countCharacters['W']], ['X', countCharacters['X']],
+			['Y', countCharacters['Y']], ['Z', countCharacters['Z']],
+		]);
+		
+		var options = {
+			title: 'Letter Frequency Analysis',
+			hAxis: {
+				title: 'Letter',
+			},
+			vAxis: {
+				title: 'Frequency'
+			}
+		};
+		
+		var chart = new google.visualization.ColumnChart(
+			document.getElementById('letters-frequency-chart'));
+		
+		chart.draw(data, options);
+	}
+	
 }
