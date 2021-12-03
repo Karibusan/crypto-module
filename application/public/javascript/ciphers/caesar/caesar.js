f11cd67d02c3881e511de937b966e605951c26b6
Yan Urquiza <yan.urquiza@gmail.com>
2021-12-03 10:26:18 +0000

    caesar cipher done. Doesn't handle positive (right shift) key

--- application/public/javascript/ciphers/caesar/caesar.js
+++ application/public/javascript/ciphers/caesar/caesar.js
@@ -2,33 +2,32 @@ $ ('#caesar-key-btn').on ('click', function () {
 	crackItJulius ();
 });
 
-function  crackItJulius()
-{
-	$ ('#caesar-result').val ();
+function crackItJulius () {
+	$ ('#caesar-result').val ('');
+	
 	let text = $ ('#text-cipher').val ();
 	let decipheredText = text;
 	let textLength = text.length;
-	let key = $ ('#caesar-key').val ();
-	let absoluteKey = Math.abs(key);
-	for(i = 0; i < textLength; i++)
-	{
-		let char = text.charAt(i);
-		let charCode = parseInt(char.charCodeAt(0));
-		if((charCode >(65 - absoluteKey) && charCode <= (90 + absoluteKey)) || (charCode >= (97 - absoluteKey) && charCode <= (122 + absoluteKey))) {
-			let newCharCode = parseInt (charCode) + (parseInt (key));
-			
-			if(newCharCode < 65)
-			{
-				let position = 65 - newCharCode;
-				newCharCode = 90 - position;
-			}
-			if(newCharCode < 97 && newCharCode > 89)
-			{
-				let position = 97 - newCharCode;
-				newCharCode = 122 - position;
+	let key = parseInt ($ ('#caesar-key').val ());
+	let absoluteKey = Math.abs (key);
+	
+	for (i = 0; i < textLength; i++) {
+		let char = text.charAt (i);
+		let charCode = parseInt (char.charCodeAt (0));
+		let newCharCode = '';
+		
+		if ((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)) {
+			if ((charCode >= 65 && charCode <= 90) && (charCode - absoluteKey) < 65) {
+				let testTemp = absoluteKey - (charCode - 64);
+				newCharCode = 90 - testTemp;
+			} else if ((charCode >= 97 && charCode <= 122) && (charCode - absoluteKey) < 97) {
+				let testTemp = absoluteKey - (charCode - 96);
+				newCharCode = 122 - testTemp;
+			} else {
+				newCharCode = charCode + key;
 			}
 			let newChar = String.fromCharCode (newCharCode);
-			decipheredText = decipheredText.substring(0,i) + newChar + decipheredText.substring(i+1);
+			decipheredText = decipheredText.substring (0, i) + newChar + decipheredText.substring (i + 1);
 		}
 	}
 	$ ('#caesar-result').val (decipheredText);
