bd2c24f3f73c7fff544fcb72829e435a7c8b44cc
Yan Urquiza <yan.urquiza@gmail.com>
2021-12-08 16:35:01 +0000

    transposition cipher

--- application/public/javascript/ciphers/transposition/simple.js
+++ application/public/javascript/ciphers/transposition/simple.js
@@ -1,5 +1,6 @@
 $ ('#text-cipher-btn').on ('click', function () {
 	console.log('transposition');
+	//document.getElementById('matches-results-ta').val('');
 	getSquareRootOrClose ();
 });
 
@@ -7,17 +8,78 @@ function getSquareRootOrClose()
 {
 	let text = $ ('#text-cipher').val ();
 	let textlength = text.length;
-	let chars = text.split('');
 	//let squareRoot = Math.ceil(Math.sqrt(textlength));
-	let squareRoot = Math.ceil(textlength / 2);
-	console.log(squareRoot);
-}
-
-function transposeIt(text, length)
-{
-	for(i = 0; i < length; i++)
-	{
+	let squareRoot = Math.ceil(textlength / 3);
+	const max_size = squareRoot;
+	const min_size = Math.floor(squareRoot / 3);
 	
+	console.log('getSquareRootOrClose' , 'max_size' , max_size);
+	console.log('getSquareRootOrClose' , 'min_size' , min_size);
+	
+	let count = max_size;
+	while (count > min_size) {
+		let splitSize = count;
+		const yardstick = new RegExp (`.{${splitSize}}`, 'g'); // /.{10}/g;
+		const pieces = text.match (yardstick);
+		const accumulated = (pieces.length * splitSize);
+		const modulo = text.length % accumulated;
+		if (modulo) pieces.push (text.slice (accumulated));
+		transposeIt (pieces);
+		//console.log('getSquareRootOrClose' , 'max_size' , max_size);
+		//console.log('getSquareRootOrClose' , 'yardstick' , yardstick);
+		console.log('getSquareRootOrClose' , 'pieces' , pieces);
+		//console.log('getSquareRootOrClose' , 'accumulated' , accumulated);
+		//console.log('getSquareRootOrClose' , 'modulo' , modulo);
+		count--;
 	}
 
+}
+
+function transposeIt(pieces)
+{
+	let piecesCount = pieces.length;
+	let pieceLength = pieces[0].length;
+	let rows = [];
+	let columns = [];
+	for(let i = 0; i < piecesCount; i++)
+	{
+		let piece = pieces[i];
+		rows[i] = piece.split('');
+		let column = [];
+		for(let j = 0; j < pieceLength; j++)
+		{
+			column.push(piece.charAt(j));
+		}
+		columns[i] = column;
+	}
+	//console.log('transposeit' , 'columns' , columns);
+	//console.log('transposeit' , 'pieces' , pieces);
+	//.log('transposeit' , 'pieces' , rows);
+	displayArray(columns, rows);
+}
+
+function displayArray(columns, rows)
+{
+	let table = document.createElement('table');
+	table.classList.add('display');
+	table.classList.add('table');
+	table.classList.add('table-borderless');
+	table.classList.add('table-active');
+	let tbody = document.createElement('tbody');
+	table.appendChild(tbody);
+	for(let i = 0; i < rows.length; i++)
+	{
+		let row = rows[i];
+		let tr = document.createElement('tr');
+		let rowLength = row.length;
+		for(let j = 0; j < rowLength; j++) {
+				let td = document.createElement ('td');
+				td.innerHTML = row[j];
+				tr.appendChild (td);
+		}
+				tbody.appendChild(tr);
+				table.appendChild(tbody);
+		
+	}
+	document.getElementById('matches-results-ta').appendChild(table);
 }
