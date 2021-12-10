e23c0787035e57a7bef352fcbaa48ac0843709f9
Yan Urquiza <yan.urquiza@gmail.com>
2021-12-10 17:43:49 +0000

    ml

---
+++ application/public/javascript/ML/caesar.js
@@ -0,0 +1,80 @@
+let learningModel1Input = "J ibe tffo mjuumf pg Ipmnft mbufmz. Nz nbssjbhf ibe esjgufe vt bxbz gspn fbdi puifs. Nz" +
+	" pxo " +
+	"dpnqmfuf ibqqjoftt, boe uif ipnf-dfousfe joufsftut xijdi sjtf vq bspvoe uif nbo xip gjstu gjoet ijntfmg nbtufs " +
+	"pg ijt pxo ftubcmjtinfou, xfsf tvggjdjfou up bctpsc bmm nz buufoujpo, xijmf Ipmnft, xip mpbuife fwfsz gpsn pg " +
+	"tpdjfuz xjui ijt xipmf Cpifnjbo tpvm, sfnbjofe jo pvs mpehjoht jo Cblfs Tusffu, cvsjfe bnpoh ijt pme " +
+	"cpplt, boe bmufsobujoh gspn xffl up xffl cfuxffo dpdbjof boe bncjujpo, uif espxtjoftt pg uif esvh, " +
+	"boe uif gjfsdf fofshz pg ijt pxo lffo obuvsf. If xbt tujmm, bt fwfs, effqmz buusbdufe cz uif tuvez pg dsjnf, " +
+	"boe pddvqjfe ijt jnnfotf gbdvmujft boe fyusbpsejobsz qpxfst pg pctfswbujpo jo gpmmpxjoh pvu uiptf dmvft, " +
+	"boe dmfbsjoh vq uiptf nztufsjft xijdi ibe cffo bcboepofe bt ipqfmftt cz uif pggjdjbm qpmjdf. " +
+	"Gspn ujnf up ujnf J ifbse tpnf wbhvf bddpvou pg ijt epjoht: pg ijt tvnnpot up Pefttb jo uif dbtf pg uif " +
+	"Usfqpgg nvsefs, pg ijt dmfbsjoh vq pg uif tjohvmbs usbhfez pg uif Buljotpo cspuifst bu Usjodpnbmff, " +
+	"boe gjobmmz pg uif njttjpo xijdi if ibe bddpnqmjtife tp efmjdbufmz boe tvddfttgvmmz gps uif sfjhojoh gbnjmz pg " +
+	"Ipmmboe. Cfzpoe uiftf tjhot pg ijt bdujwjuz, ipxfwfs, xijdi J nfsfmz tibsfe xjui bmm uif sfbefst pg uif ebjmz " +
+	"qsftt, J lofx mjuumf pg nz gpsnfs gsjfoe boe dpnqbojpo.";
+
+let learningModel1Output = "I had seen little of Holmes lately. My marriage had drifted us away from each other. My " +
+	"own complete happiness, and the home-centred interests which rise up around the man who first finds himself " +
+	"master of his own establishment, were sufficient to absorb all my attention, while Holmes, " +
+	"who loathed every form of society with his whole Bohemian soul, remained in our lodgings in Baker Street, " +
+	"buried among his old books, and alternating from week to week between cocaine and ambition, " +
+	"the drowsiness of the drug, and the fierce energy of his own keen nature. He was still, as ever, " +
+	"deeply attracted by the study of crime, and occupied his immense faculties and extraordinary powers of " +
+	"observation in following out those clues, and clearing up those mysteries which had been abandoned as hopeless " +
+	"by the official police. From time to time I heard some vague account of his doings: of his summons to Odessa " +
+	"in the case of the Trepoff murder, of his clearing up of the singular tragedy of the Atkinson brothers at " +
+	"Trincomalee, and finally of the mission which he had accomplished so delicately and successfully for the " +
+	"reigning family of Holland. Beyond these signs of his activity, however, which I merely shared with all the " +
+	"readers of the daily press, I knew little of my former friend and companion.";
+
+let learningModel2Input = "N zna ragrerq jub pbhyq uneqyl unir orra yrff guna fvk srrg fvk vapurf va urvtug, " +
+	"jvgu gur purfg naq yvzof bs n Urephyrf. Uvf qerff jnf evpu jvgu n evpuarff juvpu jbhyq, va Ratynaq, " +
+	"or ybbxrq hcba nf nxva gb onq gnfgr. Urnil onaqf bs nfgenxuna jrer fynfurq npebff gur fyrrirf naq sebagf " +
+	"bs uvf qbhoyr- oernfgrq pbng, juvyr gur qrrc oyhr pybnx juvpu jnf guebja bire uvf fubhyqref jnf " +
+	"yvarq jvgu synzrpbybherq fvyx naq frpherq ng gur arpx jvgu n oebbpu juvpu pbafvfgrq bs n fvatyr synzvat orely. " +
+	"Obbgf juvpu rkgraqrq unysjnl hc uvf pnyirf, naq juvpu jrer gevzzrq ng gur gbcf jvgu evpu oebja she, pbzcyrgrq gur " +
+	"vzcerffvba bs oneonevp bchyrapr juvpu jnf fhttrfgrq ol uvf jubyr nccrnenapr. Ur pneevrq n oebnq-oevzzrq ung va " +
+	"uvf unaq, juvyr ur jber npebff gur hccre cneg bs uvf snpr, rkgraqvat qbja cnfg gur purrxobarf, n oynpx ivmneq " +
+	"znfx, juvpu ur unq nccneragyl nqwhfgrq gung irel zbzrag, sbe uvf unaq jnf fgvyy envfrq gb vg nf ur ragrerq. " +
+	"Sebz gur ybjre cneg bs gur snpr ur nccrnerq gb or n zna bs fgebat punenpgre, jvgu n guvpx, unatvat yvc, " +
+	"naq n ybat, fgenvtug puva fhttrfgvir bs erfbyhgvba chfurq gb gur yratgu bs bofgvanpl";
+
+let learningModel2Output = "A man entered who could hardly have been less than six feet six inches in height, " +
+	"with the chest and limbs of a Hercules. His dress was rich with a richness which would, in England, " +
+	"be looked upon as akin to bad taste. Heavy bands of astrakhan were slashed across the sleeves and fronts " +
+	"of his double- breasted coat, while the deep blue cloak which was thrown over his shoulders was lined with " +
+	"flamecoloured silk and secured at the neck with a brooch which consisted of a single flaming beryl. " +
+	"Boots which extended halfway up his calves, and which were trimmed at the tops with rich brown fur, completed " +
+	"the impression of barbaric opulence which was suggested by his whole appearance. He carried a broad-brimmed hat " +
+	"in his hand, while he wore across the upper part of his face, extending down past the cheekbones, a black vizard " +
+	"mask, which he had apparently adjusted that very moment, for his hand was still raised to it as he entered. " +
+	"From the lower part of the face he appeared to be a man of strong character, with a thick, hanging lip, " +
+	"and a long, straight chin suggestive of resolution pushed to the length of obstinacy";
+
+learningModel1Input = learningModel1Input.split (' ');
+learningModel1Output = learningModel1Output.split (' ');
+
+
+let dataModel = [];
+for (i = 0; i < learningModel1Input.length; i++) {
+	let punctuationlessInput = learningModel1Input[i].replace (/[.,\/#!$%\^&\*;?:{}=\-\—_`~() ]/g, "");
+	let punctuationlessOutput = learningModel1Output[i].replace (/[.,\/#!$%\^&\*;?:{}=\-\—_`~() ]/g, "");
+	let row = {input: punctuationlessInput, output: punctuationlessOutput};
+	dataModel.push (row);
+}
+const config = {
+	//iterations: 15000,
+	log: true,
+	logPeriod: 500,
+	layers: [10],
+};
+
+// Create a Neural Network
+const network = new brain.recurrent.LSTM (config);
+network.train(dataModel, config);
+const output = network.run('bmufsobujoh');
+console.log('LM', 'output', output);
+//console.log(network.run([0,0]));
+//console.log(network.run([0,1]));
+//console.log(network.run([1,0]));
+//console.log(network.run([0,0]));
