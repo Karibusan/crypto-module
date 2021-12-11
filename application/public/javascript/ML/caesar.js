0e95ea957b44337201b4cf4cfe6c574b34cd12c7
Yan Urquiza <yan.urquiza@gmail.com>
2021-12-11 12:53:05 +0000

    updated model

--- application/public/javascript/ML/caesar.js
+++ application/public/javascript/ML/caesar.js
@@ -13,6 +13,76 @@ let learningModel1Input = "J ibe tffo mjuumf pg Ipmnft mbufmz. Nz nbssjbhf ibe e
 	"Ipmmboe. Cfzpoe uiftf tjhot pg ijt bdujwjuz, ipxfwfs, xijdi J nfsfmz tibsfe xjui bmm uif sfbefst pg uif ebjmz " +
 	"qsftt, J lofx mjuumf pg nz gpsnfs gsjfoe boe dpnqbojpo.";
 
+let learningModel2Input = "K jcf uggp nkvvng qh Jqnogu ncvgna. Oa octtkcig jcf ftkhvgf wu cyca htqo gcej qvjgt. Oa qyp eqorngvg " +
+	"jcrrkpguu, cpf vjg jqog-egpvtgf kpvgtguvu yjkej tkug wr ctqwpf vjg ocp yjq hktuv hkpfu jkougnh ocuvgt qh jku " +
+	"qyp guvcdnkujogpv, ygtg uwhhkekgpv vq cduqtd cnn oa cvvgpvkqp, yjkng Jqnogu, yjq nqcvjgf gxgta hqto " +
+	"qh uqekgva ykvj jku yjqng Dqjgokcp uqwn, tgockpgf kp qwt nqfikpiu kp Dcmgt Uvtggv, dwtkgf coqpi jku qnf " +
+	"dqqmu, cpf cnvgtpcvkpi htqo yggm vq yggm dgvyggp eqeckpg cpf codkvkqp, vjg ftqyukpguu qh vjg ftwi, cpf " +
+	"vjg hkgteg gpgtia qh jku qyp mggp pcvwtg. Jg ycu uvknn, cu gxgt, fggrna cvvtcevgf da vjg uvwfa qh etkog, " +
+	"cpf qeewrkgf jku koogpug hcewnvkgu cpf gzvtcqtfkpcta rqygtu qh qdugtxcvkqp kp hqnnqykpi qwv vjqug " +
+	"enwgu, cpf engctkpi wr vjqug oauvgtkgu yjkej jcf dggp cdcpfqpgf cu jqrgnguu da vjg qhhkekcn rqnkeg. " +
+	"Htqo vkog vq vkog K jgctf uqog xciwg ceeqwpv qh jku fqkpiu: qh jku uwooqpu vq Qfguuc kp vjg ecug qh vjg " +
+	"Vtgrqhh owtfgt, qh jku engctkpi wr qh vjg ukpiwnct vtcigfa qh vjg Cvmkpuqp dtqvjgtu cv Vtkpeqocngg, " +
+	"cpf hkpcnna qh vjg okuukqp yjkej jg jcf ceeqornkujgf uq fgnkecvgna cpf uweeguuhwnna hqt vjg " +
+	"tgkipkpi hcokna qh Jqnncpf. Dgaqpf vjgug ukipu qh jku cevkxkva, jqygxgt, yjkej K ogtgna ujctgf ykvj cnn " +
+	"vjg tgcfgtu qh vjg fckna rtguu, K mpgy nkvvng qh oa hqtogt htkgpf cpf eqorcpkqp";
+
+let learningModel3Input = "L kdg vhhq olwwoh ri Krophv odwhob. Pb pduuldjh kdg guliwhg xv dzdb iurp hdfk rwkhu. Pb rzq frpsohwh " +
+	"kdsslqhvv, dqg wkh krph-fhqwuhg lqwhuhvwv zklfk ulvh xs durxqg wkh pdq zkr iluvw ilqgv klpvhoi pdvwhu ri klv " +
+	"rzq hvwdeolvkphqw, zhuh vxiilflhqw wr devrue doo pb dwwhqwlrq, zkloh Krophv, zkr ordwkhg hyhub irup " +
+	"ri vrflhwb zlwk klv zkroh Erkhpldq vrxo, uhpdlqhg lq rxu orgjlqjv lq Ednhu Vwuhhw, exulhg dprqj klv rog " +
+	"errnv, dqg dowhuqdwlqj iurp zhhn wr zhhn ehwzhhq frfdlqh dqg dpelwlrq, wkh gurzvlqhvv ri wkh guxj, dqg " +
+	"wkh ilhufh hqhujb ri klv rzq nhhq qdwxuh. Kh zdv vwloo, dv hyhu, ghhsob dwwudfwhg eb wkh vwxgb ri fulph, " +
+	"dqg rffxslhg klv lpphqvh idfxowlhv dqg hawudruglqdub srzhuv ri revhuydwlrq lq iroorzlqj rxw wkrvh " +
+	"foxhv, dqg fohdulqj xs wkrvh pbvwhulhv zklfk kdg ehhq dedqgrqhg dv krshohvv eb wkh riilfldo srolfh. " +
+	"Iurp wlph wr wlph L khdug vrph ydjxh dffrxqw ri klv grlqjv: ri klv vxpprqv wr Rghvvd lq wkh fdvh ri wkh " +
+	"Wuhsrii pxughu, ri klv fohdulqj xs ri wkh vlqjxodu wudjhgb ri wkh Dwnlqvrq eurwkhuv dw Wulqfrpdohh, " +
+	"dqg ilqdoob ri wkh plvvlrq zklfk kh kdg dffrpsolvkhg vr gholfdwhob dqg vxffhvvixoob iru wkh " +
+	"uhljqlqj idplob ri Kroodqg. Ehbrqg wkhvh vljqv ri klv dfwlylwb, krzhyhu, zklfk L phuhob vkduhg zlwk doo " +
+	"wkh uhdghuv ri wkh gdlob suhvv, L nqhz olwwoh ri pb iruphu iulhqg dqg frpsdqlrq";
+
+let learningModel4Input = "M leh wiir pmxxpi sj Lspqiw pexipc. Qc qevvmeki leh hvmjxih yw eaec jvsq iegl sxliv. Qc sar gsqtpixi " +
+	"lettmriww, erh xli lsqi-girxvih mrxiviwxw almgl vmwi yt evsyrh xli qer als jmvwx jmrhw lmqwipj qewxiv sj lmw " +
+	"sar iwxefpmwlqirx, aivi wyjjmgmirx xs efwsvf epp qc exxirxmsr, almpi Lspqiw, als psexlih izivc jsvq " +
+	"sj wsgmixc amxl lmw alspi Fsliqmer wsyp, viqemrih mr syv pshkmrkw mr Feoiv Wxviix, fyvmih eqsrk lmw sph " +
+	"fssow, erh epxivrexmrk jvsq aiio xs aiio fixaiir gsgemri erh eqfmxmsr, xli hvsawmriww sj xli hvyk, erh " +
+	"xli jmivgi irivkc sj lmw sar oiir rexyvi. Li aew wxmpp, ew iziv, hiitpc exxvegxih fc xli wxyhc sj gvmqi, " +
+	"erh sggytmih lmw mqqirwi jegypxmiw erh ibxvesvhmrevc tsaivw sj sfwivzexmsr mr jsppsamrk syx xlswi " +
+	"gpyiw, erh gpievmrk yt xlswi qcwxivmiw almgl leh fiir eferhsrih ew lstipiww fc xli sjjmgmep tspmgi. " +
+	"Jvsq xmqi xs xmqi M lievh wsqi zekyi eggsyrx sj lmw hsmrkw: sj lmw wyqqsrw xs Shiwwe mr xli gewi sj xli " +
+	"Xvitsjj qyvhiv, sj lmw gpievmrk yt sj xli wmrkypev xvekihc sj xli Exomrwsr fvsxlivw ex Xvmrgsqepii, " +
+	"erh jmreppc sj xli qmwwmsr almgl li leh eggsqtpmwlih ws hipmgexipc erh wyggiwwjyppc jsv xli " +
+	"vimkrmrk jeqmpc sj Lspperh. Ficsrh xliwi wmkrw sj lmw egxmzmxc, lsaiziv, almgl M qivipc wlevih amxl epp " +
+	"xli viehivw sj xli hempc tviww, M oria pmxxpi sj qc jsvqiv jvmirh erh gsqtermsr";
+
+let learningModel5Input = "N mfi xjjs qnyyqj tk Mtqrjx qfyjqd. Rd rfwwnflj mfi iwnkyji zx fbfd kwtr jfhm tymjw. Rd tbs htruqjyj " +
+	"mfuunsjxx, fsi ymj mtrj-hjsywji nsyjwjxyx bmnhm wnxj zu fwtzsi ymj rfs bmt knwxy knsix mnrxjqk rfxyjw tk mnx " +
+	"tbs jxyfgqnxmrjsy, bjwj xzkknhnjsy yt fgxtwg fqq rd fyyjsynts, bmnqj Mtqrjx, bmt qtfymji jajwd ktwr " +
+	"tk xthnjyd bnym mnx bmtqj Gtmjrnfs xtzq, wjrfnsji ns tzw qtilnslx ns Gfpjw Xywjjy, gzwnji frtsl mnx tqi " +
+	"gttpx, fsi fqyjwsfynsl kwtr bjjp yt bjjp gjybjjs hthfnsj fsi frgnynts, ymj iwtbxnsjxx tk ymj iwzl, fsi " +
+	"ymj knjwhj jsjwld tk mnx tbs pjjs sfyzwj. Mj bfx xynqq, fx jajw, ijjuqd fyywfhyji gd ymj xyzid tk hwnrj, " +
+	"fsi thhzunji mnx nrrjsxj kfhzqynjx fsi jcywftwinsfwd utbjwx tk tgxjwafynts ns ktqqtbnsl tzy ymtxj " +
+	"hqzjx, fsi hqjfwnsl zu ymtxj rdxyjwnjx bmnhm mfi gjjs fgfsitsji fx mtujqjxx gd ymj tkknhnfq utqnhj. " +
+	"Kwtr ynrj yt ynrj N mjfwi xtrj aflzj fhhtzsy tk mnx itnslx: tk mnx xzrrtsx yt Tijxxf ns ymj hfxj tk ymj " +
+	"Ywjutkk rzwijw, tk mnx hqjfwnsl zu tk ymj xnslzqfw ywfljid tk ymj Fypnsxts gwtymjwx fy Ywnshtrfqjj, " +
+	"fsi knsfqqd tk ymj rnxxnts bmnhm mj mfi fhhtruqnxmji xt ijqnhfyjqd fsi xzhhjxxkzqqd ktw ymj " +
+	"wjnlsnsl kfrnqd tk Mtqqfsi. Gjdtsi ymjxj xnlsx tk mnx fhynanyd, mtbjajw, bmnhm N rjwjqd xmfwji bnym fqq " +
+	"ymj wjfijwx tk ymj ifnqd uwjxx, N psjb qnyyqj tk rd ktwrjw kwnjsi fsi htrufsnts";
+
+let learningModel6Input = "O ngj ykkt rozzrk ul Nursky rgzkre. Se sgxxogmk ngj jxolzkj ay gcge lxus kgin uznkx. Se uct iusvrkzk " +
+	"ngvvotkyy, gtj znk nusk-iktzxkj otzkxkyzy cnoin xoyk av gxuatj znk sgt cnu loxyz lotjy nosykrl sgyzkx ul noy " +
+	"uct kyzghroynsktz, ckxk yalloioktz zu ghyuxh grr se gzzktzout, cnork Nursky, cnu rugznkj kbkxe luxs " +
+	"ul yuiokze cozn noy cnurk Hunksogt yuar, xksgotkj ot uax rujmotmy ot Hgqkx Yzxkkz, haxokj gsutm noy urj " +
+	"huuqy, gtj grzkxtgzotm lxus ckkq zu ckkq hkzckkt iuigotk gtj gshozout, znk jxucyotkyy ul znk jxam, gtj " +
+	"znk lokxik ktkxme ul noy uct qkkt tgzaxk. Nk cgy yzorr, gy kbkx, jkkvre gzzxgizkj he znk yzaje ul ixosk, " +
+	"gtj uiiavokj noy ossktyk lgiarzoky gtj kdzxguxjotgxe vuckxy ul uhykxbgzout ot lurrucotm uaz znuyk " +
+	"iraky, gtj irkgxotm av znuyk seyzkxoky cnoin ngj hkkt ghgtjutkj gy nuvkrkyy he znk ulloiogr vuroik. " +
+	"Lxus zosk zu zosk O nkgxj yusk bgmak giiuatz ul noy juotmy: ul noy yassuty zu Ujkyyg ot znk igyk ul znk " +
+	"Zxkvull saxjkx, ul noy irkgxotm av ul znk yotmargx zxgmkje ul znk Gzqotyut hxuznkxy gz Zxotiusgrkk, " +
+	"gtj lotgrre ul znk soyyout cnoin nk ngj giiusvroynkj yu jkroigzkre gtj yaiikyylarre lux znk " +
+	"xkomtotm lgsore ul Nurrgtj. Hkeutj znkyk yomty ul noy gizoboze, nuckbkx, cnoin O skxkre yngxkj cozn grr " +
+	"znk xkgjkxy ul znk jgore vxkyy, O qtkc rozzrk ul se luxskx lxoktj gtj iusvgtout";
+
 let learningModel1Output = "I had seen little of Holmes lately. My marriage had drifted us away from each other. My " +
 	"own complete happiness, and the home-centred interests which rise up around the man who first finds himself " +
 	"master of his own establishment, were sufficient to absorb all my attention, while Holmes, " +
@@ -27,54 +97,25 @@ let learningModel1Output = "I had seen little of Holmes lately. My marriage had 
 	"reigning family of Holland. Beyond these signs of his activity, however, which I merely shared with all the " +
 	"readers of the daily press, I knew little of my former friend and companion.";
 
-let learningModel2Input = "N zna ragrerq jub pbhyq uneqyl unir orra yrff guna fvk srrg fvk vapurf va urvtug, " +
-	"jvgu gur purfg naq yvzof bs n Urephyrf. Uvf qerff jnf evpu jvgu n evpuarff juvpu jbhyq, va Ratynaq, " +
-	"or ybbxrq hcba nf nxva gb onq gnfgr. Urnil onaqf bs nfgenxuna jrer fynfurq npebff gur fyrrirf naq sebagf " +
-	"bs uvf qbhoyr- oernfgrq pbng, juvyr gur qrrc oyhr pybnx juvpu jnf guebja bire uvf fubhyqref jnf " +
-	"yvarq jvgu synzrpbybherq fvyx naq frpherq ng gur arpx jvgu n oebbpu juvpu pbafvfgrq bs n fvatyr synzvat orely. " +
-	"Obbgf juvpu rkgraqrq unysjnl hc uvf pnyirf, naq juvpu jrer gevzzrq ng gur gbcf jvgu evpu oebja she, pbzcyrgrq gur " +
-	"vzcerffvba bs oneonevp bchyrapr juvpu jnf fhttrfgrq ol uvf jubyr nccrnenapr. Ur pneevrq n oebnq-oevzzrq ung va " +
-	"uvf unaq, juvyr ur jber npebff gur hccre cneg bs uvf snpr, rkgraqvat qbja cnfg gur purrxobarf, n oynpx ivmneq " +
-	"znfx, juvpu ur unq nccneragyl nqwhfgrq gung irel zbzrag, sbe uvf unaq jnf fgvyy envfrq gb vg nf ur ragrerq. " +
-	"Sebz gur ybjre cneg bs gur snpr ur nccrnerq gb or n zna bs fgebat punenpgre, jvgu n guvpx, unatvat yvc, " +
-	"naq n ybat, fgenvtug puva fhttrfgvir bs erfbyhgvba chfurq gb gur yratgu bs bofgvanpl";
-
-let learningModel2Output = "A man entered who could hardly have been less than six feet six inches in height, " +
-	"with the chest and limbs of a Hercules. His dress was rich with a richness which would, in England, " +
-	"be looked upon as akin to bad taste. Heavy bands of astrakhan were slashed across the sleeves and fronts " +
-	"of his double- breasted coat, while the deep blue cloak which was thrown over his shoulders was lined with " +
-	"flamecoloured silk and secured at the neck with a brooch which consisted of a single flaming beryl. " +
-	"Boots which extended halfway up his calves, and which were trimmed at the tops with rich brown fur, completed " +
-	"the impression of barbaric opulence which was suggested by his whole appearance. He carried a broad-brimmed hat " +
-	"in his hand, while he wore across the upper part of his face, extending down past the cheekbones, a black vizard " +
-	"mask, which he had apparently adjusted that very moment, for his hand was still raised to it as he entered. " +
-	"From the lower part of the face he appeared to be a man of strong character, with a thick, hanging lip, " +
-	"and a long, straight chin suggestive of resolution pushed to the length of obstinacy";
-
-learningModel1Input = learningModel1Input.split (' ');
-learningModel1Output = learningModel1Output.split (' ');
-
-
-let dataModel = [];
-for (i = 0; i < learningModel1Input.length; i++) {
-	let punctuationlessInput = learningModel1Input[i].replace (/[.,\/#!$%\^&\*;?:{}=\-\—_`~() ]/g, "");
-	let punctuationlessOutput = learningModel1Output[i].replace (/[.,\/#!$%\^&\*;?:{}=\-\—_`~() ]/g, "");
-	let row = {input: punctuationlessInput, output: punctuationlessOutput};
-	dataModel.push (row);
-}
 const config = {
 	//iterations: 15000,
 	log: true,
 	logPeriod: 500,
-	layers: [10],
+	//layers: [10],
 };
 
 // Create a Neural Network
 const network = new brain.recurrent.LSTM (config);
-network.train(dataModel, config);
-const output = network.run('bmufsobujoh');
+//network.train(dataModel, config);
+network.train([
+	{input: learningModel1Input, output: learningModel1Output},
+	{input: learningModel2Input, output: learningModel1Output},
+	{input: learningModel3Input, output: learningModel1Output},
+	{input: learningModel4Input, output: learningModel1Output},
+	{input: learningModel5Input, output: learningModel1Output},
+	{input: learningModel6Input, output: learningModel1Output},
+], config);
+const json = network.toJSON();
+console.log('trained', json);
+const output = network.run('nbssjbhf');
 console.log('LM', 'output', output);
-//console.log(network.run([0,0]));
-//console.log(network.run([0,1]));
-//console.log(network.run([1,0]));
-//console.log(network.run([0,0]));
